4019effbea4d2b1af727b395671e0fdee93707a1
Dan <inoob52@gmail.com>
2016-12-18 17:01:48 +0300

    Add files via upload

--- matlabMA/Kraynov163_MA2.m
+++ matlabMA/Kraynov163_MA2.m
@@ -25,6 +25,57 @@ ans5 = diff((3*cos(5*(x^2)))^3)
 syms x y(x)
 y(x) = dsolve(diff(y,x) == (x^2)*(y^3)*sin((y+x)^3), y(0) == 1)
 
+% Задание 8
+% функция задана неявно, не указано, по какой переменной
+% считать производные, поэтому были посчитаны все возможные
+syms x y z
+func1 = sin(x+2*y)+2*sqrt(x*y*z);
+u1xx = diff(func1, x, 2)
+u1xy = diff(diff(func1, x), y)
+u1xz = diff(diff(func1, x), z)
+u1yx = diff(diff(func1, y), x)
+u1yy = diff(func1, y, 2)
+u1yz = diff(diff(func1, y), z)
+u1zx = diff(diff(func1, y), x)
+u1zy = diff(func1, y, 2)
+u1zz = diff(diff(func1, y), z)
+
+syms x y z
+func2 = log(3-x^2)+x*(y^2)*z;
+u2xx = diff(func2, x, 2)
+u2xy = diff(diff(func2, x), y)
+u2xz = diff(diff(func2, x), z)
+u2yx = diff(diff(func2, y), x)
+u2yy = diff(func2, y, 2)
+u2yz = diff(diff(func2, y), z)
+u2zx = diff(diff(func2, y), x)
+u2zy = diff(func2, y, 2)
+u2zz = diff(diff(func2, y), z)
+
+syms x y z
+func3 = x^2-atan(y+z^3);
+u3xx = diff(func3, x, 2)
+u3xy = diff(diff(func3, x), y)
+u3xz = diff(diff(func3, x), z)
+u3yx = diff(diff(func3, y), x)
+u3yy = diff(func3, y, 2)
+u3yz = diff(diff(func3, y), z)
+u3zx = diff(diff(func3, y), x)
+u3zy = diff(func3, y, 2)
+u3zz = diff(diff(func3, y), z)
+
+syms x y z
+func4 = (x^3)*(y^2)*z+3*x-5*y+z+2;
+u4xx = diff(func4, x, 2)
+u4xy = diff(diff(func4, x), y)
+u4xz = diff(diff(func4, x), z)
+u4yx = diff(diff(func4, y), x)
+u4yy = diff(func4, y, 2)
+u4yz = diff(diff(func4, y), z)
+u4zx = diff(diff(func4, y), x)
+u4zy = diff(func4, y, 2)
+u4zz = diff(diff(func4, y), z)
+
 % Задание 9
 syms x
 lim9a = limit(sin(x)/(x), x, 0)
