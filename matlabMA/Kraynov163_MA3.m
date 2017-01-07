fddce2f78d63681b424b0ba69c7fbf34ce808a63
Dan <inoob52@gmail.com>
2017-01-07 19:50:08 +0300

    Update Kraynov163_MA3.m

--- matlabMA/Kraynov163_MA3.m
+++ matlabMA/Kraynov163_MA3.m
@@ -1,14 +1,13 @@
-% Задание 1
-figure('name', 'Задание 1');
+% Р—Р°РґР°РЅРёРµ 1
+figure('name', 'Р—Р°РґР°РЅРёРµ 1');
 x1 = -2:0.01:2;
 y1 = exp(-x1).*sin(10*x1);
 plot(x1, y1); grid on;
 
-% Задание 2
-figure('name', 'Задание 2');
+% Р—Р°РґР°РЅРёРµ 2
+figure('name', 'Р—Р°РґР°РЅРёРµ 2');
 x2 = 0:0.001:5;
 y2 = exp(-x2).*sin(10*x2);
-
 
 subplot(2, 2, 1);
 comet(x2, y2);
@@ -23,14 +22,14 @@ subplot(2, 2, 3);
 comet3(x2, y2, z2, 0.9)
 grid on; view([-58 13]);
 
-% Задание 3
-figure('name', 'Задание 3');
+% Р—Р°РґР°РЅРёРµ 3
+figure('name', 'Р—Р°РґР°РЅРёРµ 3');
 p3 = [1 0 0 0];
 pie3([25 40 20 15], p3);
 view(120, 50);
 
-% Задание 4
-figure('name', 'Задание 4');
+% Р—Р°РґР°РЅРёРµ 4
+figure('name', 'Р—Р°РґР°РЅРёРµ 4');
 
 x4 = 0:0.01:1;
 x4_1 = 0:1/99:1;
@@ -40,8 +39,8 @@ plot(x4, y4, 'Color', 'red');
 grid on; hold on; axis equal;
 plot(x4_1, y4_1, 'Color', 'green');
 
-% Задание 5
-figure('name', 'Задание 5');
+% Р—Р°РґР°РЅРёРµ 5
+figure('name', 'Р—Р°РґР°РЅРёРµ 5');
 x5 = 0:1:10;
 y5_1 = log(2*x5);
 y5_2 = log(x5).*sin(2*x5);
@@ -51,3 +50,38 @@ loglog(x5, y5_1); hold on; axis equal; grid on;
 
 subplot(2, 2, 2);
 loglog(x5, y5_2); hold on; grid on; axis equal;
+
+% Р—Р°РґР°РЅРёРµ 6
+figure('name', 'Р—Р°РґР°РЅРёРµ 6');
+t = -pi:0.01:pi;
+x = 2*sin(t); y = 4*cos(t);
+plot(x,y);
+
+% Р—Р°РґР°РЅРёРµ 7
+figure('name', 'Р—Р°РґР°РЅРёРµ 7');
+x = -2:0.1:2; y = x;
+[X,Y]=meshgrid(x,y);
+z = 4.*sin(2*pi.*X).*cos(1.5*pi.*Y).*(1-Y.^2).*X.*(1-X);
+mesh(x,y,z);
+
+% Р—Р°РґР°РЅРёРµ 9
+figure('name', 'Р—Р°РґР°РЅРёРµ 9');
+x = -2:0.1:2; y = x;
+[X,Y]=meshgrid(x,y);
+z = 4.*sin(2*pi.*X).*cos(1.5*pi.*Y).*(1-Y.^2).*X.*(1-X);
+surf(x,y,z);
+shading interp
+lightangle(-45,30)
+view([-90 45])
+
+% Р—Р°РґР°РЅРёРµ 10
+figure('name', 'Р—Р°РґР°РЅРёРµ 10');
+x = -1:0.1:1; y = x;
+[X,Y]=meshgrid(x,y);
+z = (sin(X).^2+cos(Y).^2).^(X.*Y);
+subplot(2,2,1);
+mesh(x,y,z);
+subplot(2,2,2);
+surf(x,y,z);
+shading interp
+lightangle(-45,30)
